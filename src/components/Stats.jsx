54730fdf27a7d338d53e364501c7179c874401a8
MuhammadUsmanShafique <usman2002shafiq@gmail.com>
2024-09-05 13:46:18 +0500

    fully_functional_dynamic_attractive_travel_list_final

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,12 @@
 export default function Stats({ items }) {
+  if (items.length == 0) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 😊</em>
+      </footer>
+    );
+  }
+
   const numOfItems = items.length; // Derived state , no need to create a new state for it then we will have to remain it with sync with items length
   const noOfPackedItems = items.filter((item) => item.packed === true).length;
   const per = Math.floor((noOfPackedItems / numOfItems) * 100);
